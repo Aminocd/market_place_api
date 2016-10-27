a8b42440cba985a17e37f5986a369758a78821d7
Aminocd <Aminocd@users.noreply.github.com>
2016-10-26 18:22:48 -0700

    Adds devise user model

--- Gemfile
+++ Gemfile
@@ -59,3 +59,5 @@ gem 'active_model_serializers'
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem "devise"
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
-Rails.application.routes.draw do
-	# Api definition
-	namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
-		scope module: :v1 do
-			# We are going to list our resources here
-		end
-	end	
+require 'api_constraints'
+
+MarketPlaceApi::Application.routes.draw do
+  devise_for :users
+  # Api definition
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/'  do
+    scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+    end
+  end
 end
