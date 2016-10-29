3ecfb44418380adfc689bf2e0ce6079ecd5b3823
Amincd <amincd@users.noreply.github.com>
2016-10-28 19:44:55 -0700

    Adds shoulda matchers for spec refactors, validate_confirmation)of(:password) didn't work

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,14 @@ describe User do
 
 	it {should be_valid}
 
+	it { should validate_presence_of(:email)}
+	it { should validate_uniqueness_of(:email)}
+#	it { should validate_confirmation_of(:password)}
+	it { should allow_value('example@domain.com').for(:email)}
+
+	describe "when email is not present" do
+		before { @user.email = " "}
+		it {should_not be_valid}
+	end
+
 end
