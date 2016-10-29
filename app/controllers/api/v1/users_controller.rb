d347a116fcaa9d8498a3f80bddf94a187767ecad
Amincd <amincd@users.noreply.github.com>
2016-10-29 16:22:17 -0700

    Updates users controller to prevent CSRF exception

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::UsersController < ApplicationController
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead
+	protect_from_forgery with: :null_session
+
 	respond_to :json
 
 	def show
