3ae0dda9b45209eccf08d99063e863040c5467f2
Aminocd <Aminocd@users.noreply.github.com>
2017-02-03 07:53:58 -0800

    Creates authorize_with_token! method

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -10,7 +10,16 @@ module Authenticable
 										 status: :unauthorized unless user_signed_in? 
 	end
 
+	def authorize_with_token!
+		render json: { "errors" => "Not authorized, id: #{params[:id]}" },
+										 status: :unauthorized unless same_user_signed_in?
+	end
+
 	def user_signed_in?
 		current_user.present?
+	end
+
+	def same_user_signed_in?
+		user_signed_in? && current_user.id == params[:id].to_i
 	end
 end
