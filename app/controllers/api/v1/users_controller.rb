8013d9b949160da238dd041947f7b0594f1ed4e5
Aminocd <Aminocd@users.noreply.github.com>
2017-02-03 08:26:37 -0800

    uncomment the private scoping of user_params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -34,7 +34,7 @@ class Api::V1::UsersController < ApplicationController
 		head 204
 	end
 
-	#private
+	private
 		def user_params
 			params.require(:user).permit(:email, :password, :password_confirmation)
 		end
