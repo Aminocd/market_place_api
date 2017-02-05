515cfa86128407b46774ca6f52d11e51db7cefd0
Amincd <amincd@users.noreply.github.com>
2017-02-05 09:44:58 -0800

    Adds key param to authorize_with_token! method. Makes User, Order and Product controllers take id/user_id from URL path

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -10,16 +10,16 @@ module Authenticable
 										 status: :unauthorized unless user_signed_in? 
 	end
 
-	def authorize_with_token!
-		render json: { "errors" => "Not authorized, id: #{params[:id]}" },
-										 status: :unauthorized unless same_user_signed_in?
+	def authorize_with_token!(key)
+		render json: { "errors" => "Not authorized, user_id: #{params[key]}, key var: #{key}" },
+										 status: :unauthorized unless same_user_signed_in?(key)
 	end
 
 	def user_signed_in?
 		current_user.present?
 	end
 
-	def same_user_signed_in?
-		user_signed_in? && current_user.id == params[:id].to_i
+	def same_user_signed_in?(key)
+		user_signed_in? && current_user.id == params[key].to_i
 	end
 end
