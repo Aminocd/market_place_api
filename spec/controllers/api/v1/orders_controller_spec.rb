6b2f87d29d433278a79cd84539e5ef50d11fa76e
Aminocd <Aminocd@users.noreply.github.com>
2017-02-06 19:43:50 -0800

    Adds the show action for order

--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -1,17 +1,35 @@
 require 'spec_helper'
 
 RSpec.describe Api::V1::OrdersController, type: :controller do
-	before(:each) do
-		current_user = FactoryGirl.create :user
-		api_authorization_header current_user.auth_token
-		4.times { FactoryGirl.create :order, user: current_user }
-		get :index, user_id: current_user.id
+	describe "GET #index" do
+		before(:each) do
+			current_user = FactoryGirl.create :user
+			api_authorization_header current_user.auth_token
+			4.times { FactoryGirl.create :order, user: current_user }
+			get :index, user_id: current_user.id
+		end
+
+		it "returns 4 order records from the user" do
+			order_response = json_response[:data]
+			expect(order_response.length).to eql 4 
+		end
+
+		it { should respond_with 200}
 	end
 
-	it "returns 4 order records from the user" do
-		order_response = json_response[:data]
-		expect(order_response.length).to eql 4 
-	end
+	describe "GET #show" do
+		before(:each) do
+			current_user = FactoryGirl.create :user
+			api_authorization_header current_user.auth_token
+			@order = FactoryGirl.create :order, user: current_user
+			get :show, user_id: current_user.id, id: @order.id
+		end
 
-	it { should respond_with 200}
+		it "it returns the user order record matching the id" do
+			order_response = json_response[:data]
+			expect(order_response[:id].to_i).to eql @order.id
+		end
+
+		it { should respond_with 200 }
+	end
 end
