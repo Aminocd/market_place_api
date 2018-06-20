21a5560ccd72acaa5d6264e8c35eb383b49def0d
Aminocd <Aminocd@users.noreply.github.com>
2018-06-19 22:45:32 -0400

    Fixed migration issue

--- db/migrate/20180619160344_add_provider_and_uid_to_users.rb
+++ db/migrate/20180619160344_add_provider_and_uid_to_users.rb
@@ -1,7 +1,7 @@
 class AddProviderAndUidToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :provider, :string
-    add_column :users, :uid, :string
-    add_index :users, [:uid, :provider], unique: true
+    add_column :users, :provider, :string unless column_exists?(:users, :provider)
+    add_column :users, :uid, :string  unless column_exists?(:users, :uid)
+    add_index :users, [:uid, :provider], unique: true unless index_exists?(:users, [:uid, :provider], unique: true)
   end
 end
