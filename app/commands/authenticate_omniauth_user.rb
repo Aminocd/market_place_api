208535b8e15cfce066f4e76f44a67e57306bf6e9
Aminocd <Aminocd@users.noreply.github.com>
2018-06-25 20:42:04 -0400

    Fixed bug with omniauth resetting emails

--- app/commands/authenticate_omniauth_user.rb
+++ app/commands/authenticate_omniauth_user.rb
@@ -15,7 +15,10 @@ class AuthenticateOmniauthUser
 
   def user
     user = User.from_omniauth(auth_hash)
-    return user if user
+    if user.present?
+      errors.add :user_omniauth_authentication, user.errors.full_messages.join(" ") if user.errors.any?
+      return user
+    end
 
     errors.add :user_omniauth_authentication, 'Invalid Authorization from Provider!'
     nil
