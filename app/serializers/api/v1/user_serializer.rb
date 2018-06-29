ce25b12abbc8dd8a8c4cd420ea9238d4c7ef8195
code860 <benjaminc.barber@gmail.com>
2018-06-28 21:16:33 -0400

    Removed unecessary attribute from users serializer

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,9 +1,5 @@
 class Api::V1::UserSerializer < ActiveModel::Serializer
-  attributes :email, :created_at, :updated_at, :verified_email
+  attributes :email, :created_at, :updated_at
 
   has_many :products
-
-  def verified_email
-    object.verified_email?
-  end
 end
